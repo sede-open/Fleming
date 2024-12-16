8c2a3fc827043b3f282b03069344a8058d9cc27a
Amber-Rigg <amber.l.rigg25@gmail.com>
2024-12-16 14:55:05 +0000

    Update with formatting

--- src/fleming/code_summary/__init__.py
+++ src/fleming/code_summary/__init__.py
@@ -11,3 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .repo_contents_tokenisation import RepoContentsTokenisation
+from .fourO_mini_summary import FourOMiniSummary
--- tests/fleming/code_summary/test_fourO_mini_summary.py
+++ tests/fleming/code_summary/test_fourO_mini_summary.py
@@ -7,17 +7,25 @@ from src.fleming.code_summary.fourO_mini_summary import OpenAIClient
 
 
 def test_display_results():
-  """
-  Unit test verifies that you are getting a ValueError, and the ValueError string matches the one you're expecting
-  This is based on not running the OpenAIClient first, so you are always expecting a none dataframe
-  """  
-  openaiclient = OpenAIClient(spark_session, input_spark_df="input_spark_df", output_table_name="output_table_name", prompt="prompt", api_key="api_key", endpoint="endpoint")
-  actual_output_none = openaiclient.display_results()
-  actual_output_none_string = str(actual_output_none)
+    """
+    Unit test verifies that you are getting a ValueError, and the ValueError string matches the one you're expecting
+    This is based on not running the OpenAIClient first, so you are always expecting a none dataframe
+    """
+    openaiclient = OpenAIClient(
+        spark_session,
+        input_spark_df="input_spark_df",
+        output_table_name="output_table_name",
+        prompt="prompt",
+        api_key="api_key",
+        endpoint="endpoint",
+    )
+    actual_output_none = openaiclient.display_results()
+    actual_output_none_string = str(actual_output_none)
 
-  expected_output = ValueError("No results to display. Please call call_openai() first.")
-  expected_output_string = str(expected_output)
+    expected_output = ValueError(
+        "No results to display. Please call call_openai() first."
+    )
+    expected_output_string = str(expected_output)
 
-  assert type(actual_output_none) == type(expected_output)
-  assert actual_output_none_string == expected_output_string
-
+    assert type(actual_output_none) == type(expected_output)
+    assert actual_output_none_string == expected_output_string
